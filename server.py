a588ad0592ec5565de3d81e186a05632640471d5
Cferroz27 <cferro@uao.edu.co>
2026-02-16 15:48:25 -0500

    Update server.py

--- server.py
+++ server.py
@@ -190,10 +190,6 @@ def create_observation(observation: Observation):
 # -----------------------------------------------
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(
-        "server:app",
-        host="0.0.0.0",
-        port=8000,
-        reload=True
-    )
+    
+    app = FastAPI()
+
